97dd338509d9ca6d43b68b33e2b85116e90c1dc0
sgotou <sgotou@crooz.co.jp>
2012-08-21 15:28:30 +0900

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Samurai::Application.config.secret_token = 'a284c682c1236170be80d4c94fe7c62263e1f0db2a830f50059829e00a188763262495be46e71be3b3d11f20c679c1445005e7e453d8e900dd8567db6fd9178c'
